c8c2a39db04fc0ba3777f662f89c38600942c0c6
dymafr <erwan1309@hotmail.com>
2022-02-19 12:42:22 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template></template>
+
+<script setup lang="ts">
+const obj = { prenom: 'Jean', age: 12 };
+
+function render() {
+  console.log('Mise à jour du template par Vue.js');
+}
+
+const proxy = new Proxy(obj, {
+  get(obj, prop) {
+    render();
+    return obj[prop];
+  },
+  set(obj, prop) {
+    obj[prop] = value;
+    render();
+    return true;
+  },
+});
+
+console.log(proxy.prenom);
+</script>
+
+<style></style>
